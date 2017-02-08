172991a987b5c6ed27b239e10ed44edfd16712ac
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-02-08 11:20:57 -0500

    isbn

---
+++ isbn.rb
@@ -0,0 +1,48 @@
+require "minitest/autorun"
+require_relative "testisbn.rb"
+
+class TestIsbnNumber < Minitest::Test
+	
+	def test_1_with_valid_special_character_x_isbn10_return_true
+		isbn_num = "049567148x"
+		assert_equal(true,check_isbn_number(isbn_num))
+	end
+	def test_2_with_space_isbn10_return_true
+		isbn_num = "978 0 471 480"
+		assert_equal(true,check_isbn_number(isbn_num))
+	end
+	def test_3_with_hyphens_isbn10_return_true
+		isbn_num = "438-0465-438"
+		assert_equal(true,check_isbn_number(isbn_num))
+	end
+	def test_4_with_special_character_return_false
+		isbn_num = "584%23459884"
+		assert_equal(false,check_isbn_number(isbn_num))
+	end
+    def test_5_with_number_return_false
+    	isbn_num = "8040749853485"
+    	assert_equal(false,check_isbn_number(isbn_num))
+    end
+    def test_6_with_number_return_true
+    	isbn_num = "123456789X"
+    	assert_equal(true,check_isbn_number(isbn_num))
+    end
+#for 13
+  
+    def test_7_with_number_return_true
+    	isbn_num = "981495146601x"
+    	assert_equal(true,check_isbn_number13(isbn_num))
+    end
+    def test_8_with_number_return_false
+    	isbn_num = "4971%59143565698"
+    	assert_equal(false,check_isbn_number13(isbn_num))
+    end
+    def test_9_with_number_return_true
+    	isbn_num = "538573205832X"
+    	assert_equal(true,check_isbn_number13(isbn_num))
+    end
+    def test_10_with_number_return_false
+    	isbn_num = "9384-484-578989"
+    	assert_equal(false,check_isbn_number13(isbn_num))
+    end
+end
---
+++ testisbn.rb
@@ -0,0 +1,84 @@
+def check_isbn_number(isbn_num)
+
+	isbn_number = isbn_num.delete(' -') #delete all the hypens and spaces from (isbn_num)
+
+	return false if isbn_number.length != 10 #if isbn_number is not equal to 10 then it returns false
+
+		new_number = isbn_number[0..8] #give the new function(new_number) and to count for number
+		isbn_digit = isbn_number[9]   #give anothe function(new_digit) to count for x
+
+    if new_number.count("^0-9").zero? && isbn_digit.count("^0-9xX").zero?
+        true  #then it return true after counting all number and x in new_number
+    else
+    	return false
+    end
+end
+
+def checksum_array(isbn_num)
+  isbn_num_array = isbn_number.split('').map(&:to_i)
+    sum = 0
+    position = 1
+    isbn_num_array.each do |calc_checksum|
+    	sum += checksum*position
+    end
+
+    checksum = sum % 11
+
+    isbn_checksum = new_isbn[9]
+    if isbn_checksum == 'x' || isbn_checksum == 'X'
+    	checksum_value = 10
+    else
+    	checksum_value = isbn_checksum.to_i
+    end
+
+    if checksum == checksum_value
+    	return true
+    else
+    	return false
+    end
+end
+#for 13
+def check_isbn_number13(isbn_num)
+	new_isbn = isbn_num.delete(" -")
+
+	return false if new_isbn.length != 13
+	
+	if new_isbn.count("^0-9").zero? 
+		false
+	else 
+		return true
+	end
+end
+
+def checksum_array_13(isbn_num)
+
+	new_isbn = isbn_num[0..11]
+
+	isbn_num_array = isbn_number.split('').map(&:to_i)
+
+	position = 1
+	sum = 0
+
+	isbn_num_array.each do |calc_checksum|
+		if position.event?
+			sum += calc_checksum
+	    else
+	    	sum += calc_checksum
+	    end
+	    position += 1
+	end
+
+	checksum = (10 - sum % 10) % 10
+
+	isbn_checksum = new_isbn[12]
+	checksum_value = isbn_checksum.to_i
+
+	if checksum == checksum_value
+		return true
+	else
+		return false
+	end
+end
+
+
+
