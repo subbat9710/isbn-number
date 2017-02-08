59fdc28f9f79b8d098e9f364e684f12a8d48d336
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-02-08 11:35:16 -0500

    isbn1

---
+++ isbn2.rb
@@ -0,0 +1,97 @@
+require "minitest/autorun"
+require_relative "isbn1.rb"
+
+
+class TestISBN < Minitest::Test
+	
+	# Simple test to ensure basic shell of test file and required_relative is in place
+	def test_1_returns_1
+		assert_equal(1,1)
+	end
+
+## ISBN10 TESTING ##
+
+	def test_ISBN10_invalid_length
+		isbn_num = "0-321-14653-02"
+		assert_equal(false,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_length_special_character
+		isbn_num = "321%14653-0"
+		assert_equal(false,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_length_x_in_incorrect_position
+		isbn_num = "877195x869"
+		assert_equal(false,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_length_x_in_correct_position
+		isbn_num = "877195869x"
+		assert_equal(true,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_number_first_example
+		isbn_num = "0471958697"
+		assert_equal(true,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_number_second_example
+		isbn_num = "0-321-14653-0"
+		assert_equal(true,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_valid_number_third_example
+		isbn_num = "877195869x"
+		assert_equal(true,validate_input10(isbn_num))
+	end
+
+	def test_ISBN10_special_character_in_last_position
+		isbn_num = "877195869&"
+		assert_equal(false,validate_input10(isbn_num))
+	end
+
+## ISBN13 TESTING ## 
+
+	def test_ISBN13_valid_number_first_example
+		isbn_num = "9780470059029"
+		assert_equal(true,validate_input13(isbn_num))
+	end
+
+	def test_ISBN13_valid_number_second_example
+		isbn_num = "978-0-13-149505-0"
+		assert_equal(true,validate_input13(isbn_num))
+	end	
+
+	def test_ISBN13_valid_number_third_example
+		isbn_num = "978 0 471 48648 0"
+		assert_equal(true,validate_input13(isbn_num))
+	end
+
+	def test_ISBN13_invalid_number_first_example
+		isbn_num = "4780470059029"
+		assert_equal(false,validate_input13(isbn_num))
+	end
+
+	def test_ISBN13_invalid_number_special_character
+		isbn_num = "0-321@14653-034"
+		assert_equal(false,validate_input13(isbn_num))
+	end
+
+	def test_ISBN13_invalid_number_too_many_digits
+		isbn_num = "0-3214314653-034"
+		assert_equal(false,validate_input13(isbn_num))
+	end
+
+	def test_ISBN13_invalid_number_too_few_digits
+		isbn_num = "0-3214 4653-034"
+		assert_equal(false,validate_input13(isbn_num))
+	end
+
+# Used valid number but modified checksum
+	def test_ISBN13_invalid_checksum
+		isbn_num = "978 0 471 48648 3"
+		assert_equal(false,validate_input13(isbn_num))
+	end
+
+end
